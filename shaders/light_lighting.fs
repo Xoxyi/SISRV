8e5215dbc35b334a09c932f5d846261ec1c163a2
Davide Camino <dadi.camino@yahoo.it>
2025-02-07 22:40:45 +0100

    init pbr

--- shaders/light_lighting.fs
+++ shaders/light_lighting.fs
@@ -4,10 +4,9 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D gAlbedo;
-uniform sampler2D gReflection;
 
 void main() {
-	float type = texture(gReflection, TexCoords).a;
+	float type = texture(gAlbedo, TexCoords).a;
 	if(type < 0.4 || type >= 0.6)
 		discard;
 	vec3 color = texture(gAlbedo, TexCoords).rgb;
